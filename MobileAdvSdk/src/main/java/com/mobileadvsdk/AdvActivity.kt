a25bda0ff5b5d2d53784c75aca8e691a25a6cc1c
IvanDanilov <esseckers@gmail.com>
2022-01-06 16:23:43 +0300

    add module sdk and libraries

---
+++ MobileAdvSdk/src/main/java/com/mobileadvsdk/AdvActivity.kt
@@ -0,0 +1,11 @@
+package com.mobileadvsdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdvActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
