4e71ceb2c2d421d1cd3f8dbacd6e656ed998427f
IvanDanilov <esseckers@gmail.com>
2022-01-15 14:09:02 +0300

    add VST player

--- MobileAdvSdk/src/main/java/com/mobileadvsdk/AdvApplication.kt
+++ MobileAdvSdk/src/main/java/com/mobileadvsdk/AdvApplication.kt
@@ -2,11 +2,18 @@ package com.mobileadvsdk
 
 import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
+import com.mobileadvsdk.datasource.di.dataSourceModule
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
 
-class AdvApplication:MultiDexApplication() {
+class AdvApplication:MultiDexApplication(), KodeinAware {
 
     override fun onCreate() {
         super.onCreate()
         MultiDex.install(this)
+    }
+
+    override val kodein: Kodein by Kodein.lazy {
+        import(dataSourceModule)
     }
 }
